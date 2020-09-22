7442c0649fbce128989e985bcd1c6bc5b15d0226
Aashna1226 <70753667+Aashna1226@users.noreply.github.com>
2020-09-22 15:35:45 +0530

    Update sketch.js

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -4,7 +4,6 @@ var speed, weight;
 
 function setup() {
   createCanvas(1600,400);
-  createSprite(400, 200, 50, 50);
   car = createSprite (50, 200, 50, 50);
   wall = createSprite (1500, 200, 60, height/2);
   car.shapeColor = "red";
